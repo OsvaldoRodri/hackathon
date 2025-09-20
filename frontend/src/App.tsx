0dd605cb04becfe0cbc2d2e094ecdc1d93a38231
TurixD <arturo12loera01@gmail.com>
2025-09-19 18:56:09 -0600

    Update App layout and styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import './App.css';
+import '/styles/App.css'
 
 function App(): React.JSX.Element {
   return (
     <div className="container">
       {/* Superior Izquierdo */}
       <div className="top-left">
-        <h2>Realiza los pagos de tus servicios de luz y agua</h2>
+        <h1>Realiza los pagos de tus servicios de luz y agua</h1>
       </div>
 
       {/* Superior Derecho */}
       <div className="top-right">
-        <button>Registrar</button>
-        <button>Acceder a pagos</button>
+        <button>Soy Nuevo</button>
+        <button>Ver mis pagos</button>
       </div>
 
       {/* Inferior Izquierdo */}
@@ -23,7 +23,8 @@ function App(): React.JSX.Element {
       {/* Inferior Derecho */}
       <div className="bottom-right">
         <h3>Nosotros</h3>
-        <p>Descripción de la organización, quiénes somos y qué ofrecemos.</p>
+        <p>Somos una organización dedicada a facilitar el pago de servicios en las comunidades rurales.</p>
+        <p>Ofrecemos una plataforma sencilla y accesible para que los usuarios puedan gestionar sus pagos de manera eficiente.</p>
       </div>
     </div>
   );
