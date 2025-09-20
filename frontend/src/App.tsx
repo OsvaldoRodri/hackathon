6265ad44dfa96422a4cf35d7030584194635db33
TurixD <arturo12loera01@gmail.com>
2025-09-19 18:31:07 -0600

    Implementación de body

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
 
 function App(): React.JSX.Element {
   return (
-    <>
-      <div>
-        <h1>Vite + React</h1>
+    <div className="container">
+      {/* Superior Izquierdo */}
+      <div className="top-left">
+        <h2>Realiza los pagos de tus servicios de luz y agua</h2>
       </div>
-    </>
+
+      {/* Superior Derecho */}
+      <div className="top-right">
+        <button>Registrar</button>
+        <button>Acceder a pagos</button>
+      </div>
+
+      {/* Inferior Izquierdo */}
+      <div className="bottom-left">
+        <img src="ruta_de_tu_imagen.png" alt="Imagen ilustrativa" />
+      </div>
+
+      {/* Inferior Derecho */}
+      <div className="bottom-right">
+        <h3>Nosotros</h3>
+        <p>Descripción de la organización, quiénes somos y qué ofrecemos.</p>
+      </div>
+    </div>
   );
 }
 
-export default App
+export default App;
